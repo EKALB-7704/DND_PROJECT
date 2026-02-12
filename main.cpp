bd4b863113abe32caed2e0ad1de3df42083ec6ea
EKALB <c23395031@mytudublin.ie>
2026-02-12 16:37:20 +0000

    minor update

--- main.cpp
+++ main.cpp
@@ -10,15 +10,17 @@ using namespace std;
 
 int main()
 {
-   // Test for stats input and display functions 
-    Player_stats player_1(15,14,13,12,10,8);
 
-    player_1.setAbility(15, 1); // Check for proper changing and displaying of stats
-    cout << player_1.getAbility(1) << endl;
+// Test for stats input and display functions 
+Player_stats player_1(15,14,13,12,10,8);
+
+player_1.setAbility(15, 1); // Check for proper changing and displaying of stats
+cout << player_1.getAbility(1) << endl;
 
 
-    player_1.setAbility(26, 2); // Check for improper changing and displaying of stats 
-    cout << player_1.getAbility(2) << endl;
+player_1.setAbility(26, 2); // Check for improper changing and displaying of stats 
+cout << player_1.getAbility(2) << endl;
+
 
 
 
